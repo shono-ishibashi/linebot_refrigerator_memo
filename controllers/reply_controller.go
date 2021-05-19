26d448357b343b2bf2200d2bedbd615dd6f410cd
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2021-05-20 01:02:44 +0900

    add recipe

--- controllers/reply_controller.go
+++ controllers/reply_controller.go
@@ -279,6 +279,9 @@ func fetchRecipe(foodName string, categoryList []recipe.Category) ([][]recipe.Re
 	if len(searchedCategoryList) == 0 {
 		log.Println("no result")
 	}
+	fmt.Println("====================")
+	fmt.Println(searchedCategoryList)
+	fmt.Println("====================")
 
 	var recipeListList [][]recipe.Recipe
 	for _, searchedCategory := range searchedCategoryList {
@@ -289,9 +292,6 @@ func fetchRecipe(foodName string, categoryList []recipe.Category) ([][]recipe.Re
 
 		recipeListList = append(recipeListList, recipeList)
 	}
-	fmt.Println("====================")
-	fmt.Println(recipeListList)
-	fmt.Println("====================")
 
 	return recipeListList, nil
 }
